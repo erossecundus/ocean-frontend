e6778b5f2916843231ebf96aba27a9b32d198279
Eros Secundus <156208632+erossecundus@users.noreply.github.com>
2024-01-30 13:37:26 +0000

    implementação de props no card

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,28 @@ import Card from './components/Card/Card'
 import './App.css'
 
 function App() {
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+  const item3 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+  const item4 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+  }
+
   return (
     <>
     <div className="cards">
-      <Card />
-      <Card />
+      <Card item={item1} />
+      <Card item={item2} />
     </div>
       
     </>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,21 @@
 import './Info/Info.css'
 import Info from './Info/Info'
 
-function Card() {
+function Card(props) {
+  //console.log(props)
+
+  const item = props.item
+  //console.log(props)
   return (
       <>
           <div className="card">
-            <h2>Rick Sanchez</h2>
+            <h2>{item.name}</h2>
             <div className="infos">
               <Info />
               <Info />
               <Info />
             </div>
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+            <img src={item.image} />
           </div>
       </>
   )
