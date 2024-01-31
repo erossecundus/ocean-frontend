a6bbd3c4c00bf14b8621877e646ad980db2aaa4a
Eros Secundus <156208632+erossecundus@users.noreply.github.com>
2024-01-31 15:04:23 +0000

    correção de largura dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
 import './components/Card/Card.css'
 import Card from './components/Card/Card'
 import './App.css'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
-  //carregamento de dados via codigo
-  const item1 = {
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  }
-  const item2 = {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
-  }
-  const item3 = {
-    name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
-  const item4 = {
-    name: 'Beth Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
-  }
+  // //carregamento de dados via codigo
+  // const item1 = {
+  //   name: 'Rick Sanchez',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  // }
+  // const item2 = {
+  //   name: 'Morty Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+  // }
+  // const item3 = {
+  //   name: 'Summer Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  // }
+  // const item4 = {
+  //   name: 'Beth Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+  // }
 
-  const itens = [item1, item2, item3, item4]
+  // const itens = [item1, item2, item3, item4]
 
   //carregamento de dado via API
+  
+  const [itens, setItens] = useState([])
+  
   async function carregarDadosApi() {
     const apiURL = 'https://rickandmortyapi.com/api/character/'
     
@@ -33,11 +38,18 @@ function App() {
     //console.log(response)
     
     const body = await response.json()
-    console.log(body)
+
+    const results = body.results
+    console.log(results)
+
+    setItens(results)
 
   }
 
-  carregarDadosApi()
+  useEffect(function () {
+    carregarDadosApi()   
+  }, [])
+
 
   return (
     <>
